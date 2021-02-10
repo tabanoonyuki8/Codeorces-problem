b472aa3d5eb887cf083c3dd9456f354d45bed1aa
Tabanoon mim <74909482+tabanoonyuki8@users.noreply.github.com>
2021-02-10 12:59:12 +0600

    Add files via upload

---
+++ Codeforce1.cpp
@@ -0,0 +1,21 @@
+//Codeforces problem
+//A. Watermelon
+
+#include<iostream>
+#include<string>
+ using namespace std;
+
+ int main(){
+
+int a;
+
+cin>>a;
+if(a%2==0 && a>2){
+    cout<<"YES\n";
+
+}
+ else{
+    cout<<"No\n";
+ }
+ return 0;
+ }
